9af9146c88992b65602038a542e52f5e7e32299f
kizuyoko <kizuyoko@gmail.com>
2025-01-09 15:57:07 +0100

    Vercel fix, Mobile version's Image control

--- src/components/BackgroundControlMb.tsx
+++ src/components/BackgroundControlMb.tsx
@@ -1,18 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../store/store';
+import { setCurrentIndex } from '../store/backgroundSlice';
+import { AppDispatch } from '../store/store';
 import { Button } from "../ui/Button"
 
 export const BackgroundControlMb = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const currentIndex = useSelector((state: RootState) => state.background.currentIndex);
+  const backgrounds = useSelector((state: RootState) => state.background.backgrounds);
+
+  const handlePrev = () => {
+    const newIndex = (currentIndex - 1 + backgrounds.length) % backgrounds.length;
+    dispatch(setCurrentIndex(newIndex));
+  };
+
+  const handleNext = () => {
+    const newIndex = (currentIndex + 1) % backgrounds.length;
+    dispatch(setCurrentIndex(newIndex));
+  };
+
   return (
     <article className="box md:mr-4 cols-parent-h">
       <Button 
-        onClick={() => {}}
+        onClick={handlePrev}
         src='/arrow_l.svg'
         alt='←'
         imgClassName='w-7 h-7 flex'
       />
-      <p className='xs:hidden mx-2'>Bg</p>
-      <p className="hidden xs:block mx-2">Background</p>
+      <p className='mx-2 xs:hidden'>Bg</p>
+      <p className="hidden mx-2 xs:block">Background</p>
       <Button 
-        onClick={() => {}}
+        onClick={handleNext}
         src='/arrow_r.svg'
         alt='→'
         imgClassName='w-7 h-7 flex'
--- src/store/backgroundSlice.ts
+++ src/store/backgroundSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const apiKey = import.meta.env.VITE_BACKGROUNDIMAGE_API_KEY_UNSPLASH;
