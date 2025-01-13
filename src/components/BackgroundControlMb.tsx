484a8fa09ab11cb2fc8e91d33f61666da8d0285e
kizuyoko <kizuyoko@gmail.com>
2025-01-13 12:45:36 +0100

    Add title to mobile background setting

--- src/components/BackgroundControlMb.tsx
+++ src/components/BackgroundControlMb.tsx
@@ -28,36 +28,39 @@ export const BackgroundControlMb = () => {
   };
 
   return (
-    <article className="box md:mr-4 cols-parent-h">
-      <Button 
-        onClick={handlePrev}
-        src='/arrow_l.svg'
-        alt='←'
-        imgClassName='w-7 h-7 mr-2 flex'
-      />
-      <form
-        onSubmit={handleKeyword} 
-        className='flex w-full p-2 ml-2 border border-gray-500 rounded-md'>
-        <input 
-          type='text'
-          placeholder='Keyword'
-          className="flex-1 w-full text-sm font-normal text-center focus:outline-none"
-        />
+    <article className="w-full box">
+      <h3 className='pb-1 text-sm font-normal text-center'>Background Image Setting</h3>
+      <div className='pb-2 cols-parent-h'>
         <Button 
-          type='submit'
-          src='/search.svg'
-          alt='Search'
-          title="Search"
-          imgClassName='w-6 h-6 flex'
-          onClick={() => {}}
+          onClick={handlePrev}
+          src='/arrow_l.svg'
+          alt='←'
+          imgClassName='w-7 h-7 mr-2 flex'
         />
-      </form>
-      <Button 
-        onClick={handleNext}
-        src='/arrow_r.svg'
-        alt='→'
-        imgClassName='w-7 h-7 ml-2 flex'
-      />
+        <form
+          onSubmit={handleKeyword} 
+          className='flex w-full p-2 ml-2 border border-gray-500 rounded-md'>
+          <input 
+            type='text'
+            placeholder='Keyword'
+            className="flex-1 w-full text-sm font-normal text-center focus:outline-none"
+          />
+          <Button 
+            type='submit'
+            src='/search.svg'
+            alt='Search'
+            title="Search"
+            imgClassName='w-6 h-6 flex'
+            onClick={() => {}}
+          />
+        </form>
+        <Button 
+          onClick={handleNext}
+          src='/arrow_r.svg'
+          alt='→'
+          imgClassName='w-7 h-7 ml-2 flex'
+        />
+      </div>
     </article>
   );
 };
